61516d49ba32b2b4301489edb9662776128dbddb
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-16 11:45:36 -0300

    unit tests ok

--- front-end/src/app/app.component.spec.ts
+++ front-end/src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -7,6 +9,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule
+      ]
     }).compileComponents();
   });
 
@@ -20,12 +25,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ponto-inteligente');
-  });
-
-  it('should render button', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('button')).toBeTruthy;
   });
 });
