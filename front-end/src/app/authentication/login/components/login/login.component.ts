bdfdfb1be41f60b37b428355e8b8d9a06d60fa2c
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-16 17:58:54 -0300

    create generateForm()

--- front-end/src/app/authentication/login/components/login/login.component.ts
+++ front-end/src/app/authentication/login/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public form!: FormGroup;
+
+  constructor (
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.generateForm();
+  }
+
+  public generateForm(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
 }
--- front-end/src/app/authentication/login/login.module.ts
+++ front-end/src/app/authentication/login/login.module.ts
@@ -1,5 +1,13 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 
 import { LogarComponent, LoginComponent } from './components';
@@ -11,7 +19,15 @@ import { LogarComponent, LoginComponent } from './components';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule
   ]
 })
 export class LoginModule { }
