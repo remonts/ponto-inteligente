183b615534bcead9669f04c1d731e8e99b550c52
Renan Monteiro <monteiro-renan@hotmail.com>
2023-01-16 10:30:43 -0300

    solved unit test

--- front-end/src/app/app.component.spec.ts
+++ front-end/src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ponto-inteligente');
   });
 
-  it('should render title', () => {
+  it('should render button', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ponto-inteligente app is running!');
+    expect(compiled.querySelector('button')).toBeTruthy;
   });
 });
